6fb0f459dfd6986dbf6aa5021cf8011d38760638
Moe Abe <71622151+k19rs003@users.noreply.github.com>
2024-04-07 23:21:08 +0900

    Create: チャット画面作成

---
+++ Chatwork/Views/ChatViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+
+        return cell
+    }
+    
+}
