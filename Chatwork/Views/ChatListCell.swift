180406ba472df85185a45f7a5fdf6ecc4b0c7f38
Moe Abe <k23gjk01@st.kyusan-u.ac.jp>
2024-04-08 18:05:16 +0900

    Add: 画像を取得するライブラリの追加

--- Chatwork/Views/ChatListCell.swift
+++ Chatwork/Views/ChatListCell.swift
@@ -1,11 +1,21 @@
+import Nuke
+import NukeExtensions
 import UIKit
 
 class ChatListCell: UITableViewCell {
 
+    @IBOutlet weak var iconLabel: UIImageView! {
+        didSet {
+            iconLabel.layer.cornerRadius = iconLabel.frame.height / 2
+        }
+    }
+
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var unReadLabel: UILabel!
     
-    func create(name: String, unread: Int) {
+    func create(image: String, name: String, unread: Int) {
+        NukeExtensions.loadImage(with: URL(string: image), into: self.iconLabel)
+        iconLabel.image = UIImage(named: image)
         nameLabel.text = name
         unReadLabel.text = "未読：\(String(unread))件"
     }
